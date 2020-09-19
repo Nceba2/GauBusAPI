a029c7e759aea0ea5baa11f6a2b5bb77d0a0eec3
Nceba2 <ncebasobikwa1@hotmail.com>
2020-09-19 20:32:54 +0200

    first commit: adding a starting point of the api

---
+++ src/main/java/com/gaubusapi/GauBusAPI/GauBusApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaubusapi.GauBusAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GauBusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GauBusApiApplication.class, args);
+	}
+
+}
