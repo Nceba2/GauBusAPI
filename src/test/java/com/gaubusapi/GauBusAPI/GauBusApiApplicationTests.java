2d321b814998e7c8f4628e25e51701643a8fa90e
Nceba2 <ncebasobikwa1@hotmail.com>
2020-09-19 20:33:42 +0200

    first commit: adding a starting point of the api

---
+++ src/test/java/com/gaubusapi/GauBusAPI/GauBusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaubusapi.GauBusAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GauBusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
